a254159f18847f617819ed84e892207deebb4542
George Kokinis <george.kokinis@gmail.com>
2022-03-09 18:50:26 +0000

    Remove existing action bar; attempt to add our own. Not working :(.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/MainActivity.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/MainActivity.kt
@@ -2,10 +2,26 @@ package uk.fisherthewol.smartkart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ab: Toolbar = findViewById(R.id.ActionBar)
+        setSupportActionBar(ab)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.SettingsButton -> {
+            // TODO: Intent to settings window.
+            Toast.makeText(applicationContext, "Pressed Button", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
