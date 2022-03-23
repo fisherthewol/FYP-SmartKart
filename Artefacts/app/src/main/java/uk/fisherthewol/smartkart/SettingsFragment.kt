8a456f980bcd0fa91d0e6b59826a9341da3d0c0b
George Kokinis <george.kokinis@gmail.com>
2022-03-23 21:13:42 +0000

    Change to SeekBar.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
@@ -1,20 +1,18 @@
 package uk.fisherthewol.smartkart
 
 import android.os.Bundle
-import android.text.InputType
-import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SeekBarPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.fragment_settings, rootKey)
         // Limit input on predict time.
-        val modelPredict: EditTextPreference? = findPreference("model_predict_time")
-        val minBound = resources.getInteger(R.integer.predict_min_bound)
-        val maxBound = resources.getInteger(R.integer.predict_max_bound)
-        modelPredict?.setOnBindEditTextListener {
-                EditText -> EditText.inputType = InputType.TYPE_CLASS_NUMBER
-                             EditText.filters = arrayOf(ModelPredictFilter(minBound, maxBound)) }
+        val modelPredict: SeekBarPreference? = findPreference("model_predict_time")
+        if (modelPredict != null) {
+            modelPredict.max = resources.getInteger(R.integer.predict_max_bound)
+            modelPredict.min = resources.getInteger(R.integer.predict_min_bound)
+        }
     }
 
 }
