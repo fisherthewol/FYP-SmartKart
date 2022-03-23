6d95fb59e14254c5d5030e5336ab0abc54961fbc
George Kokinis <george.kokinis@gmail.com>
2022-03-23 21:29:31 +0000

    Remove bounds, because something is wrong.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
@@ -2,17 +2,10 @@ package uk.fisherthewol.smartkart
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SeekBarPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.fragment_settings, rootKey)
-        // Limit input on predict time.
-        val modelPredict: SeekBarPreference? = findPreference("model_predict_time")
-        if (modelPredict != null) {
-            modelPredict.max = resources.getInteger(R.integer.predict_max_bound)
-            modelPredict.min = resources.getInteger(R.integer.predict_min_bound)
-        }
     }
 
 }
