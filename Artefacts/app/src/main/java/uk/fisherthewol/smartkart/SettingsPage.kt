c7cbb59327935d6d3e4b1037fe76f49cdcc2edf6
George Kokinis <george.kokinis@gmail.com>
2022-03-23 15:15:50 +0000

    Load settingsFragment in settingsPage.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsPage.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsPage.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class SettingsPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainerView, SettingsFragment())
+            .commit()
+
         setContentView(R.layout.activity_settings_page)
     }
 }
