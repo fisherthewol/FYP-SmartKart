1d5de89b0ea2c3edec97fee2bfdf4597563c82b4
George Kokinis <george.kokinis@gmail.com>
2022-03-23 15:09:28 +0000

    Add SettingsFragment.

---
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package uk.fisherthewol.smartkart
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey)
+    }
+
+}
