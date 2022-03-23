aecff634e95fe2073b3eb93172e6665c2fad61d9
George Kokinis <george.kokinis@gmail.com>
2022-03-23 21:49:31 +0000

    Bounded seekbar. Yay.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/SettingsFragment.kt
@@ -2,10 +2,18 @@ package uk.fisherthewol.smartkart
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SeekBarPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.fragment_settings, rootKey)
+        val predictBar: SeekBarPreference? = findPreference("model_predict_time")
+        if (predictBar != null) {
+            predictBar.seekBarIncrement = 10
+            predictBar.setDefaultValue(10)
+            predictBar.max = resources.getInteger(R.integer.predict_max_bound)
+            predictBar.min = resources.getInteger(R.integer.predict_min_bound)
+        }
     }
 
 }
