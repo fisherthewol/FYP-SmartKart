1a3e899c1f109e31ba2046e1612d1e0d6bbc37ef
George Kokinis <george.kokinis@gmail.com>
2022-04-10 23:53:11 +0100

    Reimplement average speed to be calculated on update. Autogenerate permission check to implement one layer up.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
@@ -4,32 +4,45 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 
-class AverageSpeedModel(val locationMan: LocationManager): LocationListener {
+class AverageSpeedModel(locationMan: LocationManager): LocationListener {
     private val locations: MutableList<Location> = emptyList<Location>().toMutableList()
     init {
+        // TODO: Implement this one level above.
+        /*if (ActivityCompat.checkSelfPermission(
+                conForPermissionCheck,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                conForPermissionCheck,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }*/
         locationMan.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
-            0,
-            5.0f,
+            0, // Minimum time between updates.
+            5.0f, // Minimum distance between updateds.
             this)
     }
 
     /**
      * Calculate average speed of device.
      *
-     * Depends on [Location.getSpeed] existing on the locations.
+     * Depends on [Location.hasSpeed] == true.
      *
-     * @return Average speed in m/s. TODO: Check actual units.
+     * @return Average speed in m/s.
      */
-    val averageSpeed: Double
-        get() = when {
-            locations.isEmpty() -> 0.0
-            else -> locations.map { it.speed }.average()
-        }
+    public var averageSpeed: Double = 0.0
 
+    /**
+     * On location change, check if we can get a speed; if so, add it to the list,
+     * and calculate average speed.
+     */
     override fun onLocationChanged(loc: Location) {
         if (loc.hasSpeed()) {
             this.locations.add(loc)
+            this.averageSpeed = this.locations.map { it.speed }.average()
         }
     }
 }
