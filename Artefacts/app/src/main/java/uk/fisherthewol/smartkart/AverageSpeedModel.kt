33115d1d01ff8d000caef9525430e03ef1612dbf
George Kokinis <george.kokinis@gmail.com>
2022-04-10 22:11:06 +0100

    Refactor to registering the model as a listener.
    Consider caching value instead of calculating on fly (may be a premature optimisation).
    
    Need to check for permission - pass reference into constructor just to get context?

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
@@ -1,11 +1,18 @@
 package uk.fisherthewol.smartkart
 
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
-import androidx.core.util.Consumer
 
-class AverageSpeedModel(private val locationMan: LocationManager) {
-    private val values: MutableList<Location> = emptyList<Location>().toMutableList()
+class AverageSpeedModel(val locationMan: LocationManager): LocationListener {
+    private val locations: MutableList<Location> = emptyList<Location>().toMutableList()
+    init {
+        locationMan.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            0,
+            5.0f,
+            this)
+    }
 
     /**
      * Calculate average speed of device.
@@ -16,16 +23,13 @@ class AverageSpeedModel(private val locationMan: LocationManager) {
      */
     val averageSpeed: Double
         get() = when {
-            values.isEmpty() -> 0.0
-            else -> values.filter { it.hasSpeed() }.map { it.speed }.average()
+            locations.isEmpty() -> 0.0
+            else -> locations.map { it.speed }.average()
         }
 
-    /**
-     * Consumer to execute getCurrentLocation with.
-     *
-     * Working from https://stuff.mit.edu/afs/sipb/project/android/docs/training/basics/location/locationmanager.html
-     */
-    val locationConsumer = Consumer<Location> {
-        this.values.add(it)
+    override fun onLocationChanged(loc: Location) {
+        if (loc.hasSpeed()) {
+            this.locations.add(loc)
+        }
     }
 }
