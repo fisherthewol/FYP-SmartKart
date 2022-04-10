d467d435ac713a45f28ff0f10828b6e00df4ed87
George Kokinis <george.kokinis@gmail.com>
2022-04-10 19:59:10 +0100

    Add documentation to averageSpeed.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
@@ -6,6 +6,13 @@ import android.location.LocationManager
 class AverageSpeedModel(private val locationMan: LocationManager) {
     private val values: Array<Location> = emptyArray()
 
+    /**
+     * Calculate average speed of device.
+     *
+     * Depends on [Location.getSpeed] existing on the locations.
+     *
+     * @return Average speed in m/s. TODO: Check actual units.
+     */
     val averageSpeed: Double
         get() = when {
                 values.isEmpty() -> 0.0
