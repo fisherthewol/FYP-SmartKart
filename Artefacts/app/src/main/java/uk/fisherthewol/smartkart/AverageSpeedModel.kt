f877352e5ac778f7b22d24dcf8017dd4101f7ea6
George Kokinis <george.kokinis@gmail.com>
2022-04-10 21:16:05 +0100

    Add locationConsumer.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
@@ -2,9 +2,10 @@ package uk.fisherthewol.smartkart
 
 import android.location.Location
 import android.location.LocationManager
+import androidx.core.util.Consumer
 
 class AverageSpeedModel(private val locationMan: LocationManager) {
-    private val values: Array<Location> = emptyArray()
+    private val values: MutableList<Location> = emptyList<Location>().toMutableList()
 
     /**
      * Calculate average speed of device.
@@ -15,7 +16,16 @@ class AverageSpeedModel(private val locationMan: LocationManager) {
      */
     val averageSpeed: Double
         get() = when {
-                values.isEmpty() -> 0.0
-                else -> values.filter { it.hasSpeed() }.map { it.speed }.average()
-            }
+            values.isEmpty() -> 0.0
+            else -> values.filter { it.hasSpeed() }.map { it.speed }.average()
+        }
+
+    /**
+     * Consumer to execute getCurrentLocation with.
+     *
+     * Working from https://stuff.mit.edu/afs/sipb/project/android/docs/training/basics/location/locationmanager.html
+     */
+    val locationConsumer = Consumer<Location> {
+        this.values.add(it)
+    }
 }
