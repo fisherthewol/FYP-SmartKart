7f32c1a0f5d94de994bee30be2a5f3fa8620cbff
George Kokinis <george.kokinis@gmail.com>
2022-04-11 21:46:23 +0100

    Request location permissions on main activity creation.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModel.kt
@@ -3,27 +3,23 @@ package uk.fisherthewol.smartkart
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.util.Log
 
+/***
+ * Class to model average speed.
+ */
 class AverageSpeedModel(locationMan: LocationManager): LocationListener {
     private val locations: MutableList<Location> = emptyList<Location>().toMutableList()
     init {
-        // TODO: Implement this one level above.
-        /*if (ActivityCompat.checkSelfPermission(
-                conForPermissionCheck,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                conForPermissionCheck,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-
-            return
-        }*/
-        locationMan.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            0, // Minimum time between updates.
-            5.0f, // Minimum distance between updateds.
-            this)
+        try {
+            locationMan.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0, // Minimum time between updates.
+                5.0f, // Minimum distance between updateds.
+                this)
+        } catch (e: SecurityException) {
+            Log.e("AverageSpeedModel", "Activity creating model did not appropriately grep permission.", e)
+        }
     }
 
     /**
@@ -33,7 +29,7 @@ class AverageSpeedModel(locationMan: LocationManager): LocationListener {
      *
      * @return Average speed in m/s.
      */
-    public var averageSpeed: Double = 0.0
+    var averageSpeed: Double = 0.0
 
     /**
      * On location change, check if we can get a speed; if so, add it to the list,
--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/MainActivity.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/MainActivity.kt
@@ -1,12 +1,17 @@
 package uk.fisherthewol.smartkart
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+    var model: AverageSpeedModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportFragmentManager
@@ -16,6 +21,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val ab: Toolbar = findViewById(R.id.ActionBar)
         setSupportActionBar(ab)
+
+        // Attempt to get location permission:
+        checkForLocationPermissions()
+    }
+
+    private fun checkForLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request to get permissions.
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                55 // TODO: Check whether we need to save this value.
+            )
+        } else {
+            if (this.model == null) {
+                this.model =
+                    AverageSpeedModel(this.getSystemService(LOCATION_SERVICE) as LocationManager)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        TODO("Check for permissions then add model.")
     }
 
     /*fun settingsClick(view: View) {
