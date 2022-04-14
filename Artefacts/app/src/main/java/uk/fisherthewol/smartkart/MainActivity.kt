7b14d46402cdb64a53eb6e41fa611f6e0bab863f
George Kokinis <george.kokinis@gmail.com>
2022-04-14 01:19:56 +0100

    Enable/disable buttons. Cleanup some stuff.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/MainActivity.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/MainActivity.kt
@@ -6,9 +6,11 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private var model: AverageSpeedModel? = null
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         if (checkForLocationPermissions()) {
             if (model == null) {
                 model = AverageSpeedModel(this.getSystemService(Context.LOCATION_SERVICE) as LocationManager)
+                findViewById<Button>(R.id.StartButton).isEnabled = true
             }
         } else {
             requestLocationPermissions(this)
@@ -52,9 +55,20 @@ class MainActivity : AppCompatActivity() {
             // We're dealing with a request to show permissions. From https://github.com/googlearchive/android-RuntimePermissions/blob/96612da3a0b4489fdf818a624a7e26fc768c65c9/kotlinApp/app/src/main/java/com/example/android/system/runtimepermissions/extensions/CollectionsExts.kt
             // All granted permissions.
             if (grantResults.all { it == PackageManager.PERMISSION_GRANTED } && model == null) {
-                model = AverageSpeedModel(this.getSystemService(Context.LOCATION_SERVICE) as LocationManager)
+                model =
+                    AverageSpeedModel(this.getSystemService(Context.LOCATION_SERVICE) as LocationManager)
+                // Enable button.
+                findViewById<Button>(R.id.StartButton).isEnabled = true
+            } else {
+                // Show snackbar.
+                Snackbar.make(
+                    findViewById(R.id.mainActivityLayout),
+                    getString(R.string.noLocationPermsSB),
+                    Snackbar.LENGTH_LONG)
+                    .show()
+                // Disable button.
+                findViewById<Button>(R.id.StartButton).isEnabled = false
             }
-            // TODO: Display snackbar complaining about no permissions; disable start button.
         } else {
             // Other requests.
             super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -74,7 +88,7 @@ class MainActivity : AppCompatActivity() {
         ) == PackageManager.PERMISSION_GRANTED)
     }
 
-    //TODO: Get a setting button on top bar. Consider drawer??
+    // TODO: Get a setting button on top bar. Consider drawer??
 
     companion object {
         // Request code for getting location permissions.
