2c3c403d212fdfe24252505ff79656cbedd92a17
George Kokinis <george.kokinis@gmail.com>
2022-04-19 23:25:46 +0100

    Enable binding inside of the DashboardFragment.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/DashboardFragment.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/DashboardFragment.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import uk.fisherthewol.smartkart.databinding.FragmentDashboardBinding
 
 /**
  * Fragment subclass representing the current average speed.
- * Use the factory method to create an instance of this fragment.
  */
 class DashboardFragment : Fragment() {
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +22,12 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
