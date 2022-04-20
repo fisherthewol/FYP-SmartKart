68bd5b922807130e24768bce339320f89065726f
George Kokinis <george.kokinis@gmail.com>
2022-04-20 04:35:54 +0100

    Observer changes to average speed and update digit text.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/DashboardFragment.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/DashboardFragment.kt
@@ -1,20 +1,21 @@
 package uk.fisherthewol.smartkart
 
+import android.content.Context
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import uk.fisherthewol.smartkart.databinding.FragmentDashboardBinding
 
 /**
  * Fragment subclass representing the current average speed.
  */
-class DashboardFragment() : Fragment(), Observer<Boolean> {
+class DashboardFragment() : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+    private lateinit var model: AverageSpeedModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +26,16 @@ class DashboardFragment() : Fragment(), Observer<Boolean> {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        // Set up model with observers etc.
+        this.model = AverageSpeedModel(this.requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager)
+        model.getAverageSpeed().observe(viewLifecycleOwner) { value ->
+            binding.averageSpeedDigits.text = value.toString()
+        }
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onChanged(t: Boolean?) {
-        TODO("Not yet implemented")
     }
 }
