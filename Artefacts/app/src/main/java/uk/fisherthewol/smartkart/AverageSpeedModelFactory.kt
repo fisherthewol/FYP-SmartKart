359c3b611ef3304767678992579c19bdc5a3e5dc
George Kokinis <george.kokinis@gmail.com>
2022-04-24 22:52:03 +0100

    Set default speedLimit to 30 MPH.

--- Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModelFactory.kt
+++ Artefacts/app/src/main/java/uk/fisherthewol/smartkart/AverageSpeedModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class AverageSpeedModelFactory(private val locMan: LocationManager, private val speedLimit: MutableLiveData<Int> = MutableLiveData(0)): ViewModelProvider.Factory {
+class AverageSpeedModelFactory(private val locMan: LocationManager, private val speedLimit: MutableLiveData<Int> = MutableLiveData(30)): ViewModelProvider.Factory {
     // https://developer.android.com/codelabs/kotlin-android-training-view-model#7, https://medium.com/koderlabs/viewmodel-with-viewmodelprovider-factory-the-creator-of-viewmodel-8fabfec1aa4f
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AverageSpeedModel::class.java)) {
