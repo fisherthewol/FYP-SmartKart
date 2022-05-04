66616fdfb791856615c676cb280ad6e90c7d0381
George Kokinis <george.kokinis@gmail.com>
2022-05-04 17:44:47 +0100

    Add unit test.

---
+++ Artefacts/app/src/test/java/uk/fisherthewol/smartkart/ModelUnitTest.kt
@@ -0,0 +1,33 @@
+package uk.fisherthewol.smartkart
+
+import android.location.Location
+import android.location.LocationManager
+import androidx.arch.core.executor.TaskExecutor
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+@RunWith(MockitoJUnitRunner::class)
+class ModelUnitTest {
+    /* Adapated from https://medium.com/pxhouse/unit-testing-with-mutablelivedata-22b3283a7819 G Venios 2017.*/
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    @Test
+    fun `Single location update gets correct AverageSpeed`() {
+        val mockLocationManager: LocationManager = mock()
+        val modelUnderTest = AverageSpeedModel(mockLocationManager)
+        val singleLocation: Location = mock {
+            on {hasSpeed()} doReturn true
+            on {speed} doReturn 11.0F
+        }
+        modelUnderTest.onLocationChanged(singleLocation)
+        assertEquals(11.0 , modelUnderTest.getAverageSpeed().value)
+    }
+}
